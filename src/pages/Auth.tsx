d019b312780c1221ad8d8fa9e7d39a45ce4984b4
Vaishnavi Patil <vaishnavip276@gmail.com>
2025-03-10 20:04:50 +0530

    commit

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,13 +1,15 @@
-import image3 from "../assets/images/image-3.jpg"
+import image3 from "../assets/images/image-3.jpg";
 
 const AuthPage = () => {
   return (
     <div className="flex items-center justify-center overflow-hidden h-screen w-full">
       <div className="flex-1 border border-black h-full">
-        <img src={image3} alt="" className="w-full h-full object-cover"/>
+        <img src={image3} alt="" className="w-full h-full object-cover" />
       </div>
       <div className="flex-1 border border-black h-full flex item-center justify-center">
-        <div className="w-3/5 h-auto border border-gray p-4"></div>
+        <div className="w-3/5 h-auto border border-gray p-4 flex flex-col items-center">
+          <span className="text-4xl font-bold">Trendoria</span>
+        </div>
       </div>
     </div>
   );
