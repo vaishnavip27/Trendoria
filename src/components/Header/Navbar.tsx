05ee6afb23dfd84fa7370e5598357a02fa973e42
Vaishnavi Patil <vaishnavip276@gmail.com>
2025-03-11 11:16:17 +0530

    commit

---
+++ src/components/Header/Navbar.tsx
@@ -0,0 +1,21 @@
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between px-14 py-3">
+      <div className="flex items-center gap-20 text-sm font-semibold ">
+        <div>COLLECTIONS</div>
+        <div>ABOUT</div>
+      </div>
+      <div className="flex items-center text-3xl font-black">
+        <div>TRENDORIA</div>
+      </div>
+      <div className="flex items-center gap-20 text-sm font-semibold ">
+        <div>SEARCH</div>
+        <button className="bg-black text-white py-2.5 px-8 font-normal hover:bg-gray-800 cursor-pointer">
+          LOG IN
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
